cda68e624555790c37f2d03c0ad5a06eb0618af1
MuskStark <gitman@outlook.sg>
2024-04-23 15:42:00 +0800

    feat: 新增跨域配置和拦截器

---
+++ src/main/java/fan/summer/hmoneta/config/SpringUserInterceptor.java
@@ -0,0 +1,34 @@
+package fan.summer.hmoneta.config;
+
+import fan.summer.homelab.common.interceptor.LogInterceptor;
+import fan.summer.homelab.common.interceptor.UserInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Phoebej
+ * @date 2023/5/22
+ */
+@Component
+public class SpringUserInterceptor implements WebMvcConfigurer {
+
+    @Resource
+    private LogInterceptor logInterceptor;
+
+    @Resource
+    private UserInterceptor userInterceptor;
+
+    /**
+     * 注册自定义拦截器，拦截顺序与registry顺序一致
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+        registry.addInterceptor(userInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/register")
+                .excludePathPatterns("/login");
+    }
+}
