6d1c609b9e2f8ef26998d73fed76c8a9711ab393
MuskStark <gitman@outlook.sg>
2024-04-23 15:40:49 +0800

    feat: 新增启动log功能

--- src/main/java/fan/summer/hmoneta/HMonetaApplication.java
+++ src/main/java/fan/summer/hmoneta/HMonetaApplication.java
@@ -1,13 +1,24 @@
 package fan.summer.hmoneta;
 
+import cn.hutool.core.util.RandomUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class HMonetaApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HMonetaApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(HMonetaApplication.class, args);
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        SpringApplication application = new SpringApplication(HMonetaApplication.class);
+        Environment env = application.run(args).getEnvironment();
+        LOG.info("服务启动成功!");
+        LOG.info("启动成功，项目地址：http://localhost:{}", env.getProperty("server.port"));
     }
 
 }
