44d2afefdf4d0e5f897ec5a5da9950970d01528f
MuskStart <138420547+MuskStark@users.noreply.github.com>
2024-04-23 23:13:12 +0800

    feat(back): 增加IpPool相关查询功能

--- src/main/java/fan/summer/hmoneta/service/IpResourceManagerService.java
+++ src/main/java/fan/summer/hmoneta/service/IpResourceManagerService.java
@@ -6,6 +6,8 @@ import fan.summer.hmoneta.database.repository.IpPoolUsedDetailRepository;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * 管理HMoneta系统中IP资源
  *
@@ -34,6 +36,37 @@ public class IpResourceManagerService {
     public IpPool findIpPoolByPoolId(Long poolId) {
         return ipPoolRepository.findByPoolId(poolId);
     }
+
+    /**
+     * 根据池名查找IP池。
+     *
+     * @param poolName IP池的名称。
+     * @return 返回与指定池名匹配的IP池对象，如果没有找到，则返回null。
+     */
+    public IpPool findIpPoolByPoolName(String poolName) {
+        return ipPoolRepository.findByPoolName(poolName);
+    }
+
+    /**
+     * 根据网络地址查找所有的IP池。
+     *
+     * @param networkAddress 网络地址，用于查询关联的IP池。
+     * @return 返回一个包含所有匹配网络地址的IP池的列表。
+     */
+    public List<IpPool> findAllByNetworkAddress(String networkAddress){
+        return ipPoolRepository.findAllByNetworkAddress(networkAddress);
+    }
+
+    /**
+     * 查找并返回所有IP池的名称列表。
+     *
+     * @return List<String> 所有IP池的名称列表，以字符串列表形式返回。
+     */
+    public List<String> findAllPoolName() {
+        return ipPoolRepository.findAllPoolName();
+    }
+
+
 
 
 }
