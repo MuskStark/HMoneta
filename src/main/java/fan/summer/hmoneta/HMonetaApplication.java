5c1525a4033c5196ab3ab0c8f53e449611a6fd55
MuskStark <gitman@outlook.sg>
2024-04-23 15:07:17 +0800

    Initial commit

---
+++ src/main/java/fan/summer/hmoneta/HMonetaApplication.java
@@ -0,0 +1,13 @@
+package fan.summer.hmoneta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HMonetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HMonetaApplication.class, args);
+    }
+
+}
