414d6864e690fa2e74668804bf3aa94e636753a3
MuskStart <138420547+MuskStark@users.noreply.github.com>
2024-04-24 21:30:23 +0800

    feat(back): 增加serverInfo服务批量删除的功能

--- src/main/java/fan/summer/hmoneta/service/ServerInfoManagerService.java
+++ src/main/java/fan/summer/hmoneta/service/ServerInfoManagerService.java
@@ -1,5 +1,9 @@
 package fan.summer.hmoneta.service;
 
+import fan.summer.hmoneta.database.repository.ServerInfoDetailRepository;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
 /**
  * 管理HMoneta系统中服务器资源
  *
@@ -7,5 +11,21 @@ package fan.summer.hmoneta.service;
  * @version 1.00
  * @Date 2024/4/23
  */
+@Service
 public class ServerInfoManagerService {
+
+    @Resource
+    private ServerInfoDetailRepository serverInfoDetailRepository;
+
+    /**
+     * 根据池ID删除所有关联的服务器信息。
+     *
+     * @param poolId 池ID，用于标识要删除的服务器信息所属的池。
+     */
+    public void deleteAllByPoolId(Long poolId) {
+        if(serverInfoDetailRepository.findAllByPoolId(poolId).isEmpty()) {
+            serverInfoDetailRepository.deleteAllByPoolId(poolId);
+        }
+    }
+
 }
