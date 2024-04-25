87d7b4f9aa392c7e0c00dee27e1f8ec36cc64222
MuskStark <gitman@outlook.sg>
2024-04-25 16:38:45 +0800

    feat(back): 实现服务器信息新增功能异常提醒

--- src/main/java/fan/summer/hmoneta/common/enums/WebControllerExceptionEnum.java
+++ src/main/java/fan/summer/hmoneta/common/enums/WebControllerExceptionEnum.java
@@ -5,6 +5,10 @@ public enum WebControllerExceptionEnum {
     WEB_IP_POOL_REQ_ID_EMPTY(1002,"IP池请求ID不能为空"),
     WEB_IP_FORMAT_ERROR(1003,"IP池地址信息格式错误"),
     WEB_IP_NOT_IN_SAME_NETWORK(1004,"IP池地址不在同一个网络段"),
+    WEB_SM_REQ_EMPTY(1005,"服务器请求不能为空"),
+    WEB_SM_REQ_MAC_FORMAT_ERROR(1006,"服务器MAC地址格式错误"),
+    WEB_SM_REQ_IP_FORMAT_ERROR(1007,"服务器IP地址格式错误"),
+    WEB_SM_REQ_IP_IS_NOT_IN_POOL(1008,"服务器IP地址不在IP池中"),
     ;
     private int code;
     private String message;
