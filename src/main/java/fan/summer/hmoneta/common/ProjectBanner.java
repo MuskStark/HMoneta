0cfc719798f10d0b4b061fd75b9da07675fa5f51
MuskStark <gitman@outlook.sg>
2024-05-19 22:19:58 +0800

    feat(back): 新增项目启动banner

---
+++ src/main/java/fan/summer/hmoneta/common/ProjectBanner.java
@@ -0,0 +1,32 @@
+package fan.summer.hmoneta.common;
+
+import org.springframework.boot.Banner;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
+
+/**
+ * Banner
+ *
+ * @author phoebej
+ * @version 1.00
+ * @Date 2024/5/19
+ */
+public class ProjectBanner implements Banner {
+    private static final String BANNER =
+            " _    _  __  __                           _        \n" +
+                    "| |  | ||  \\/  |                         | |       \n" +
+                    "| |__| || \\  / | ___  _ __ ___   ___  ___| |_ __ _ \n" +
+                    "|  __  || |\\/| |/ _ \\| '_ ` _ \\ / _ \\/ __| __/ _` |\n" +
+                    "| |  | || |  | | (_) | | | | | |  __/ (__| || (_| |\n" +
+                    "|_|  |_||_|  |_|\\___/|_| |_| |_|\\___|\\___|\\__\\__,_|\n" +
+                    "                                                  \n" +
+                    "                    HMoneta-Agent (ver)               \n";
+    @Override
+    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+        String version = environment.getProperty("hmoneta.version");
+        assert version != null;
+        String banner = BANNER.replace("ver", version);
+        out.print(banner);
+    }
+}
