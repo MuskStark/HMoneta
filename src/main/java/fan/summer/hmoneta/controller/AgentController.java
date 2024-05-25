06ed70d3f87d50b0145e8c85eb9e946ea8322485
MuskStark <gitman@outlook.sg>
2024-05-25 20:39:13 +0800

    feat: 新增获取服务器状态Api

--- src/main/java/fan/summer/hmoneta/controller/AgentController.java
+++ src/main/java/fan/summer/hmoneta/controller/AgentController.java
@@ -1,13 +1,12 @@
 package fan.summer.hmoneta.controller;
 
+import fan.summer.hmoneta.database.entity.agent.AgentReport;
 import fan.summer.hmoneta.service.AgentService;
 import fan.summer.hmoneta.webEntity.agent.SystemInfoEntity;
 import fan.summer.hmoneta.webEntity.common.ApiRestResponse;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 为Agent提供专属Api接口
@@ -16,12 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version 1.00
  * @Date 2024/5/12
  */
-@Controller
-@RequestMapping("/agent/api")
+@RestController
+@RequestMapping("/master")
 public class AgentController {
 
     @Resource
     private AgentService agentService;
+
+    @GetMapping("/get-report")
+    public ApiRestResponse<Object> getAgentReport(@RequestParam Long serverId) {
+        AgentReport agentReportByServerId = agentService.findAgentReportByServerId(serverId);
+        return ApiRestResponse.success(agentReportByServerId);
+    }
 
     @PostMapping("/report")
     public ApiRestResponse<Object> receiveAgentStatus(@RequestBody SystemInfoEntity info) {
