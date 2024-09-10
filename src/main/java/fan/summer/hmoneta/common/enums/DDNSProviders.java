febf10e5076bdbd907c4036e70062e36e3a4d6b4
MuskStark <gitman@outlook.sg>
2024-09-10 10:58:05 +0800

    refactor: 修改DDNS供应商枚举类，以适应前端Select

--- src/main/java/fan/summer/hmoneta/common/enums/DDNSProviders.java
+++ src/main/java/fan/summer/hmoneta/common/enums/DDNSProviders.java
@@ -1,14 +1,19 @@
 package fan.summer.hmoneta.common.enums;
 
 public enum DDNSProviders {
-    TencentCloud("tencent");
+    TencentCloud("TencentCloud", "腾讯云");
 
     private String name;
+    private String label;
 
-    DDNSProviders(String name) {
+    DDNSProviders(String name,  String label) {
         this.name = name;
+        this.label = label;
     }
     public String getName() {
         return name;
+    }
+    public String getLabel() {
+        return label;
     }
 }
