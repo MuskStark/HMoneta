22aae7a5c758c7246d750a128e258db8fdd664e6
MuskStart <138420547+MuskStark@users.noreply.github.com>
2024-12-07 10:44:37 +0800

    test: 增加test

---
+++ src/test/java/fan/summer/hmoneta/service/ddns/DDNSServiceTest.java
@@ -0,0 +1,20 @@
+package fan.summer.hmoneta.service.ddns;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DDNSServiceTest {
+
+    @Resource
+    private DDNSService ddnsService;
+
+    @Test
+    protected void testService(){
+        ddnsService.createDdns("tencent", "summer.fan", "test");
+    }
+
+}
