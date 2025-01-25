49c90dd6bb25658a8afef8f092c70b06172219a5
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-01-26 00:28:52 +0800

    refactor: 变更类名

--- src/main/java/fan/summer/hmoneta/database/repository/ddns/DDNSInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package fan.summer.hmoneta.database.repository.ddns;
-
-import fan.summer.hmoneta.database.entity.ddns.DDNSInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DDNSInfoRepository extends JpaRepository<DDNSInfo,Long> {
-    DDNSInfo findByProviderName(String providerName);
-
-}
