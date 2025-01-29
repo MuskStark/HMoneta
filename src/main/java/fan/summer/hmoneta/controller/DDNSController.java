5a4a003539ea2c79879a4feb83c4a0167072c2b2
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-01-29 23:41:18 +0800

    refactor: 变更使用ProviderService

--- src/main/java/fan/summer/hmoneta/controller/DDNSController.java
+++ src/main/java/fan/summer/hmoneta/controller/DDNSController.java
@@ -3,10 +3,11 @@ package fan.summer.hmoneta.controller;
 import cn.hutool.core.bean.BeanUtil;
 import fan.summer.hmoneta.common.enums.DDNSProvidersSelectEnum;
 import fan.summer.hmoneta.common.enums.error.BusinessExceptionEnum;
-import fan.summer.hmoneta.database.entity.ddns.DDNSInfo;
+import fan.summer.hmoneta.database.entity.ddns.DDNSProviderEntity;
 import fan.summer.hmoneta.database.entity.ddns.DDNSRecorderEntity;
 import fan.summer.hmoneta.database.entity.ddns.DDNSUpdateRecorderEntity;
 import fan.summer.hmoneta.service.ddns.DDNSService;
+import fan.summer.hmoneta.service.ddns.provider.ProviderService;
 import fan.summer.hmoneta.util.EncryptionUtil;
 import fan.summer.hmoneta.webEntity.common.ApiRestResponse;
 import fan.summer.hmoneta.webEntity.req.ddns.DDNSProviderInfoReq;
@@ -36,14 +37,16 @@ public class DDNSController {
 
     private final Logger LOG = LoggerFactory.getLogger(this.getClass());
     private DDNSService ddnsService;
+    private final ProviderService providerService;
     private final Map<String, String> rsaKey = EncryptionUtil.generateKeyPair();
 
     @Autowired
-    public DDNSController(DDNSService ddnsService) throws Exception {
+    public DDNSController(DDNSService ddnsService, ProviderService providerService) throws Exception {
         try {
             this.ddnsService = ddnsService;
-        }catch (Exception e){
-            LOG.error("初始化失败生成RSA密钥对失败:{}",e.getMessage());
+            this.providerService = providerService;
+        } catch (Exception e) {
+            LOG.error("初始化失败生成RSA密钥对失败:{}", e.getMessage());
         }
     }
 
@@ -54,31 +57,30 @@ public class DDNSController {
 
     @GetMapping("/provider/selector")
     public ApiRestResponse<List<ProviderSelectorInfo>> getProviders() {
-        return ApiRestResponse.success(ddnsService.getProviderSelectorInfo());
+        return ApiRestResponse.success(providerService.getProviderSelectorInfo());
     }
 
     @GetMapping("/provider/query")
     public ApiRestResponse<List<ProviderInfoResp>> queryAllDDNSProvider() {
-        List<ProviderInfoResp> providerInfoResps = ddnsService.queryAllDDNSProvider();
+        List<ProviderInfoResp> providerInfoResps = providerService.queryAllDDNSProvider();
         // TODO: 空值判断
-        if(providerInfoResps == null || providerInfoResps.isEmpty()){
+        if (providerInfoResps == null || providerInfoResps.isEmpty())
             return ApiRestResponse.error(BusinessExceptionEnum.DDNS_PROVIDER_LIST_EMPTY);
-        }else {
-            for (ProviderInfoResp providerInfoResp : providerInfoResps) {
+        else {
+            for (ProviderInfoResp providerInfoResp : providerInfoResps)
                 providerInfoResp.setLabel(DDNSProvidersSelectEnum.valueOf(providerInfoResp.getProviderName()).getLabel());
-            }
             return ApiRestResponse.success(providerInfoResps);
         }
     }
 
     @PostMapping("/provider/add")
     public ApiRestResponse<Object> addDDNSProvider(@RequestBody DDNSProviderInfoReq providerInfoReq) throws Exception {
-        DDNSInfo ddnsInfo = new DDNSInfo();
-        ddnsInfo.setProviderName(providerInfoReq.getProviderName());
-        ddnsInfo.setAccessKeyId(providerInfoReq.getAccessKeyId());
+        DDNSProviderEntity ddnsProviderEntity = new DDNSProviderEntity();
+        ddnsProviderEntity.setProviderName(providerInfoReq.getProviderName());
+        ddnsProviderEntity.setAccessKeyId(providerInfoReq.getAccessKeyId());
         String accessKey = new String(EncryptionUtil.decrypt(rsaKey.get("privateKey"), providerInfoReq.getAccessKeySecret()), StandardCharsets.UTF_8);
-        ddnsInfo.setAccessKeySecret(accessKey);
-        ddnsService.modifyDdnsProvider(ddnsInfo);
+        ddnsProviderEntity.setAccessKeySecret(accessKey);
+        providerService.modifyDdnsProvider(ddnsProviderEntity);
         return ApiRestResponse.success();
     }
 
@@ -88,8 +90,9 @@ public class DDNSController {
         ddnsService.modifyDdnsRecorder(ddnsRecorderEntity);
         return ApiRestResponse.success();
     }
+
     @GetMapping("/record")
-    public ApiRestResponse<List<DDNSUpdateRecorderResp>> queryRecordInfoByProviderName(@RequestParam("providerName") String providerName){
+    public ApiRestResponse<List<DDNSUpdateRecorderResp>> queryRecordInfoByProviderName(@RequestParam("providerName") String providerName) {
         List<DDNSUpdateRecorderEntity> ddnsUpdateRecorderEntities = ddnsService.queryAllDDNSUpdateRecorderByProviderName(providerName);
         return ApiRestResponse.success(BeanUtil.copyToList(ddnsUpdateRecorderEntities, DDNSUpdateRecorderResp.class));
     }
