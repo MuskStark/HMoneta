f64796a6279355bebdcbc373a68d44372b7b6b0d
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-01-30 20:21:20 +0800

    refactor: 更改传入参数的类型

--- src/main/java/fan/summer/hmoneta/service/ddns/provider/ProviderFactory.java
+++ src/main/java/fan/summer/hmoneta/service/ddns/provider/ProviderFactory.java
@@ -1,13 +1,15 @@
 package fan.summer.hmoneta.service.ddns.provider;
 
 import fan.summer.hmoneta.common.enums.DDNSProvidersSelectEnum;
+import fan.summer.hmoneta.common.enums.error.BusinessExceptionEnum;
+import fan.summer.hmoneta.common.exception.BusinessException;
 import fan.summer.hmoneta.database.entity.ddns.DDNSProviderEntity;
 import fan.summer.hmoneta.service.ddns.provider.providers.Tencent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * 类的详细说明
+ * 获取指定DDNS供应商实例
  *
  * @author phoebej
  * @version 1.00
@@ -23,10 +25,20 @@ public class ProviderFactory {
         this.providerService = service;
     }
 
-    public DDNSProvider generatorProvider(String providerName) {
-        if (DDNSProvidersSelectEnum.valueOf(providerName) == DDNSProvidersSelectEnum.TencentCloud) {
-            DDNSProviderEntity providerEntity = providerService.getProviderEntity(providerName);
-            return new Tencent(providerEntity.getAccessKeyId(), providerEntity.getAccessKeySecret());
-        } else throw new RuntimeException("不支持的DDNS服务商");
+    /**
+     * Generates a DDNS provider instance based on the selected enumeration.
+     *
+     * @param providersEnum The enumeration representing the chosen DDNS provider. Must not be null.
+     *                      Currently, only {@link DDNSProvidersSelectEnum#TencentCloud} is supported.
+     * @return An instance of a DDNS provider subclass corresponding to the provided selection.
+     * For {@link DDNSProvidersSelectEnum#TencentCloud}, an instance of {@link Tencent} is returned.
+     * @throws BusinessException If the specified DDNS provider is not supported, with the error code
+     *                           {@link BusinessExceptionEnum#DDNS_PROVIDER_NOT_SUPPORT}.
+     */
+    public DDNSProvider generatorProvider(DDNSProvidersSelectEnum providersEnum) {
+        if (providersEnum == DDNSProvidersSelectEnum.TencentCloud) {
+            DDNSProviderEntity providerBaseInfo = providerService.getProviderBaseInfo(providersEnum.getName());
+            return new Tencent(providerBaseInfo.getAccessKeyId(), providerBaseInfo.getAccessKeySecret());
+        } else throw new BusinessException(BusinessExceptionEnum.DDNS_PROVIDER_NOT_SUPPORT);
     }
 }
