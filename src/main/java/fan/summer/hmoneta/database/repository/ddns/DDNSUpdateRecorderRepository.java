b48a74d7229b11bf41ff66e71fb4929a98c2650e
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-02-02 00:28:31 +0800

    feat: 新增按DDNS记录ID查询更新记录和删除相应的更新记录

--- src/main/java/fan/summer/hmoneta/database/repository/ddns/DDNSUpdateRecorderRepository.java
+++ src/main/java/fan/summer/hmoneta/database/repository/ddns/DDNSUpdateRecorderRepository.java
@@ -4,9 +4,14 @@ import fan.summer.hmoneta.database.entity.ddns.DDNSUpdateRecorderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DDNSUpdateRecorderRepository extends JpaRepository<DDNSUpdateRecorderEntity, Long> {
     DDNSUpdateRecorderEntity findByDomainAndSubDomain(String domain, String subDomain);
+
     List<DDNSUpdateRecorderEntity> findAllByProviderName(String providerName);
 
+    Optional<DDNSUpdateRecorderEntity> findByRecorderId(Long recorderId);
+
+    void deleteByRecorderId(Long recorderId);
 }
