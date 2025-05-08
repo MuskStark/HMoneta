6a916fb905c7c49ec114a10d62889eae02dd3083
MuskStart <gitmain@outlook.sg>
2025-05-08 11:02:48 +0800

    ✨:增加Token有效性验证

--- src/main/java/fan/summer/hmoneta/common/interceptor/UserInterceptor.java
+++ src/main/java/fan/summer/hmoneta/common/interceptor/UserInterceptor.java
@@ -25,22 +25,32 @@ public class UserInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        LOG.info(">>>>>>>>>>> UserInterceptor开始设置本地线程变量 >>>>>>>>>>>");
-        //获取header的token参数
-        String token = request.getHeader("token");
-        if (StrUtil.isNotBlank(token)) {
-            LOG.info("获取用户登录token：{}", token);
-            JSONObject loginUser = JwtUtil.getJSONObject(token);
-            LOG.info("当前登录用户：{}", loginUser);
-            UserResp userResp = JSONUtil.toBean(loginUser, UserResp.class);
-            LoginUserContext.setMember(userResp);
-            LOG.info("<<<<<<<<<<< UserInterceptor本地线程变量设置完毕 <<<<<<<<<<<");
-            return true;
-        }else{
-            LOG.info("请求中不包含token");
-            LOG.info("<<<<<<<<<<< UserInterceptor本地线程变量设置失败 <<<<<<<<<<<");
-            return false;
+        try {
+            LOG.info("-----------开始用户合法性校验-----------");
+            //获取header的token参数
+            String token = request.getHeader("token");
+            if (StrUtil.isNotBlank(token)) {
+                LOG.info("获取用户登录token：{}", token);
+                LOG.info("开始检查Token合法性");
+                if (JwtUtil.validate(token)) {
+                    LOG.info(">>>>>>>>>>> UserInterceptor开始设置本地线程变量 >>>>>>>>>>>");
+                    JSONObject loginUser = JwtUtil.getJSONObject(token);
+                    LOG.info("当前登录用户：{}", loginUser);
+                    UserResp userResp = JSONUtil.toBean(loginUser, UserResp.class);
+                    LoginUserContext.setMember(userResp);
+                    LOG.info("<<<<<<<<<<< UserInterceptor本地线程变量设置完毕 <<<<<<<<<<<");
+                    return true;
+                } else {
+                    LOG.error("非法Token，不允许访问");
+                    return false;
+                }
+            } else {
+                LOG.error("请求中不包含token");
+                return false;
+            }
+        } finally {
+            LOG.info("-----------完成用户合法性校验-----------");
         }
     }
-
 }
+
