b2c374cd1397115990e09f6612e17cac573647b3
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-05-11 10:11:54 +0800

    ♻️:使用通用下载方法替换原下载代码

--- web/src/views/SecondaryRoutingViews/acme/acme.vue
+++ web/src/views/SecondaryRoutingViews/acme/acme.vue
@@ -3,33 +3,13 @@
 // 查询全部DNS信息
 import axios from "axios";
 import {message} from "ant-design-vue";
+import {getDownloadFileFromBase64} from "@/utils/common.js";
 
 const test = () => {
   axios.get("/acme/getCert?domain=test7.summer.fan").then((resp) => {
     const json = resp.data
     if (json.status) {
-      // 将 Base64 字符串转换为二进制数据
-      const zipBytesBase64 = json.data;
-      const byteCharacters = atob(zipBytesBase64); // 解码 Base64
-      const byteNumbers = new Array(byteCharacters.length);
-
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-      }
-
-      // 将字节数组转换为 Uint8Array
-      const byteArray = new Uint8Array(byteNumbers);
-
-      const blob = new Blob([byteArray], {type: 'application/zip'});
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'test7.summer.fan.zip')
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(url)
-
+      getDownloadFileFromBase64(json.data, 'test7.summer.fan.zip', 'application/zip')
     } else {
       message.error(json.message)
     }
@@ -40,7 +20,7 @@ const test = () => {
 </script>
 
 <template>
-  <h1>证书申请界面</h1>
+
   <a-button @click="test">Test</a-button>
 </template>
 
