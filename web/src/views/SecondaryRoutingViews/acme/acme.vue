d8e05f25f4be5d581ef96dc6d0ed7372375c12aa
MuskStart <gitmain@outlook.sg>
2025-05-12 16:33:30 +0800

    ✨:增加域名证书盛情情况Table

--- web/src/views/SecondaryRoutingViews/acme/acme.vue
+++ web/src/views/SecondaryRoutingViews/acme/acme.vue
@@ -6,6 +6,13 @@ import {message} from "ant-design-vue";
 import {responseIsSuccess} from "@/utils/common.js";
 import {onMounted, reactive, ref} from "vue";
 
+const queryCertApplyStatus = (taskId) => {
+  axios.get("/acme/apply/status?taskId" + taskId).then((resp) => {
+    if (responseIsSuccess(resp)) {
+      formSource.value = resp.data.data;
+    }
+  })
+}
 // Dns供应商选择器
 const dnsList = ref([])
 const queryDDNSProvider = async () => {
@@ -32,6 +39,7 @@ const handelOk = () => {
       if (responseIsSuccess(resp)) {
         const json = resp.data.data;
         message.info("已提交证书申请，详情请查看日志")
+        queryCertApplyStatus(json)
       } else {
         message.error(resp.data.message)
       }
@@ -58,6 +66,24 @@ const formObject = reactive(
       dnsProvider: null
     }
 )
+const formSource = ref()
+const columns = [
+  {
+    title: '域名',
+    dataIndex: 'domain',
+    key: 'domain',
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: '操作',
+    dataIndex: 'operation',
+    key: 'operation',
+  },
+]
 
 
 onMounted(() => {
@@ -74,7 +100,7 @@ onMounted(() => {
   >
     <a-form>
       <a-form-item label="请输入域名">
-        <a-input v-model:value(v-model)="formObject.domain"/>
+        <a-input v-model:value="formObject.domain"/>
       </a-form-item>
       <a-form-item label="选择域名供应商">
         <a-select
@@ -82,10 +108,22 @@ onMounted(() => {
             style="width: 120px"
             :options="dnsList"></a-select>
       </a-form-item>
-
     </a-form>
   </a-modal>
   <a-button @click="openModal">申请证书</a-button>
+  <!-- 证书申请展示Table -->
+  <a-table :data-source="formSource" :columns="columns">
+    <template #bodyCell="{column, record}">
+      <template v-if="column.dataIndex === 'status'">
+        <a-tag color="green" v-if="record.status === '1'">申请成功</a-tag>
+        <a-tag color="red" v-else-if="record.status === '-1'">申请失败</a-tag>
+      </template>
+      <template v-if="column.dataIndex === 'operation'">
+        <a-button>重新申请</a-button>
+        <a-button>查看日志</a-button>
+      </template>
+    </template>
+  </a-table>
 </template>
 
 <style scoped>
